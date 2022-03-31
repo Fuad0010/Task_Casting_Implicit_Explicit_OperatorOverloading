921a0557b9cea4275633312931835c387ea03c6b
Fuad0010 <fuadft@code.edu.az>
2022-03-31 17:51:17 +0400

    Add project files.

---
+++ Task_Casting_Implicit_Explicit_OperatorOverloading/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Task_Casting_Implicit_Explicit_OperatorOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        enum Currency
+        {
+            Usd,
+            Eur,
+            Try
+        }
+
+        public static double Exchange(Enum Currency1, double azn)
+        {
+            if (Currency1 is Currency)
+            {
+                switch(Currency1)
+                {
+                    case Currency.Usd:
+                        {
+                            azn /= 1.7;
+                        }
+                        break;
+                    case Currency.Eur:
+                        {
+                            azn *= 2;
+                        }
+                        break;
+                    case Currency.Try:
+                        {
+                            azn *= 1.8;
+                        }
+                        break;
+                }   
+            }return azn;
+        }
+
+    }
+}
